cba1109e8a9495011bec3a62d480f4a8120bbfc4
evgeniydolgov <amyr214519@gmail.com>
2022-06-29 17:52:32 +0300

    init: pass the test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = n * -1;
+    }
+    let strN = String(n);
+    let result = '';
+    i = 0;
+
+    while (i < strN.length) {
+        const answer = strN[i];
+        result = `${answer}${result}`;
+        i += 1;
+    }
+    return Number(result);
+
+
 }
